5216c31e4ab98d5ee3eb49e54ab4dcaf452e059b
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2024-10-26 17:45:09 +0530

    Html form and add tailwind cdn

--- login-register/registration.php
+++ login-register/registration.php
@@ -3,9 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registration form</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <div></div>
+    <div class="container">
+        <form action="registration.php" method ="post">
+            <div class="form-group">
+                <input type="text" name ="fullname" placeholder = "Full name">
+            </div>
+
+            <div class="form-group">
+                <input type="email" name ="fullname" placeholder = "Email">
+            </div>
+
+            <div class="form-group">
+                <input type="password" name ="password" placeholder = "Password">
+            </div>
+
+            <div class="form-group">
+                <input type="password" name ="repeat_password" placeholder = "Repeat Password">
+            </div>
+
+            <div class="form-group">
+                <input type="submit" name ="submit1" value = "Register">
+            </div>
+            
+            
+        </form>
+    </div>
 </body>
 </html>
