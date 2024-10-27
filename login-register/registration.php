7477bb4bd732384c2545a84627d5822396ac7ce6
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2024-10-27 11:48:58 +0530

    connect to mysql successfully

--- login-register/registration.php
+++ login-register/registration.php
@@ -4,29 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration form</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
         <form action="registration.php" method ="post">
             <div class="form-group">
-                <input type="text" name ="fullname" placeholder = "Full name">
+                <input type="text" name ="fullname" placeholder = "Full name" class = "form-control">
             </div>
 
             <div class="form-group">
-                <input type="email" name ="fullname" placeholder = "Email">
+                <input type="email" name ="fullname" placeholder = "Email" class = "form-control">
             </div>
 
             <div class="form-group">
-                <input type="password" name ="password" placeholder = "Password">
+                <input type="password" name ="password" placeholder = "Password" class = "form-control">
             </div>
 
             <div class="form-group">
-                <input type="password" name ="repeat_password" placeholder = "Repeat Password">
+                <input type="password" name ="repeat_password" placeholder = "Repeat Password" class = "form-control">
             </div>
 
-            <div class="form-group">
-                <input type="submit" name ="submit1" value = "Register">
+            <div class="form-btn">
+                <input type="submit" name ="submit1" value = "Register" class = "btn btn-primary">
             </div>
             
             
